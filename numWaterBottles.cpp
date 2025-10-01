a6ac56ee5bb7bf0471e844b63d9d3b2087134182
KomalEncardio <gumma.sri@encardio.com>
2025-10-01 14:55:18 +0530

    Single line mathematical solution for numWaterBottles question

--- numWaterBottles.cpp
+++ numWaterBottles.cpp
@@ -4,13 +4,6 @@ using namespace std;
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int total_drank = numBottles;
-        int empty_bottles = numBottles;
-        while(empty_bottles>=numExchange){
-            int new_full = empty_bottles / numExchange;  
-            total_drank += new_full;                     
-            empty_bottles = new_full + (empty_bottles % numExchange); 
-        }
-        return  total_drank;
+        return (numBottles*numExchange-1)/(numExchange-1);
     }
 };
